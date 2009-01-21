bfa440fa030bdd2ae2e813007947636f93b1b2d7
Christian Widemann <christian.wiedemann@key-tec.de>
2009-01-21 17:22:58 +0000

    *** empty log message ***
    git-svn-id: http://svn.key-tec.de/php-maven/trunk/org.phpmaven.multimaster/org.phpmaven.plugin@70 196a8957-b169-413b-a811-b409cacd026c

--- src/test/bat/versions.bat
+++ src/test/bat/versions.bat
@@ -1,3 +1,3 @@
-SET PHPMAVENVERSION=1.0-BETA-1
+SET PHPMAVENVERSION=1.0-BETA-2
 del my-app /S /Q 
 rem C:\de.keytec.dev\cw\y\workbenches\maventest\org.phpmaven.multimaster\org.phpmaven.plugin\src\test\bat\php4-lib.bat
