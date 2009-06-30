63739fbcb8ff6a4cdd643b7a5e0ee3fc22e4f751
Christian Widemann <christian.wiedemann@key-tec.de>
2009-06-30 13:23:36 +0000

    *** empty log message ***
    git-svn-id: http://svn.key-tec.de/php-maven/trunk/org.phpmaven.multimaster/org.phpmaven.plugin@103 196a8957-b169-413b-a811-b409cacd026c

--- src/main/java/org/phpmaven/plugin/build/ExecutionError.java
+++ src/main/java/org/phpmaven/plugin/build/ExecutionError.java
@@ -2,8 +2,17 @@ package org.phpmaven.plugin.build;
 
 public class ExecutionError extends Exception {
 	
+	private String message;
+
+	public ExecutionError(String message) {
+		this.message = message;
+	}
+	public ExecutionError() {
+		this.message = "Execution of the script failed. ";
+	}
+
 	public String getMessage() {
-		return "Execution of the script failed. ";
+		return message;
 	}
 
 	private static final long serialVersionUID = 1L;
--- src/main/java/org/phpmaven/plugin/build/script/ScriptTestRunnerMojo.java
+++ src/main/java/org/phpmaven/plugin/build/script/ScriptTestRunnerMojo.java
@@ -2,7 +2,11 @@ package org.phpmaven.plugin.build.script;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+import java.util.Map.Entry;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -30,6 +34,12 @@ public class ScriptTestRunnerMojo extends AbstractMojo {
 	private ArrayList<Exception> errors = new ArrayList<Exception>();
 	
 	/**
+	 * Environment Variables.
+	 * 
+	 * @parameter
+	 */
+	private Properties environmentVariables;
+	/**
 	 * The script source folder.
 	 * 
 	 * @parameter expression="/src/main/script"
@@ -47,7 +57,6 @@ public class ScriptTestRunnerMojo extends AbstractMojo {
 	 * The command will be executed at startup
 	 * 
 	 * @parameter expression=""
-	 * @required
 	 */
 	private String startupCommand;
 	/**
@@ -91,32 +100,38 @@ public class ScriptTestRunnerMojo extends AbstractMojo {
 	protected File baseDir;
 	private String includeDirectory;
 
+
 	public String replaceCommandArgs(String command,String arg,String value) { 
 		String maskedArg = "\\*\\{"+arg+"\\}";
 		command = command.replaceAll(maskedArg, value);
 		return command;
 	}
-	private String doDefaultReplaces(String command) { 
+	private String doDefaultReplaces(String command) {
+		
 		command = replaceCommandArgs(command, "includeDirectory", baseDir+includeDirectory);
 		command = replaceCommandArgs(command, "sourceDirectory", baseDir+sourceDirectory);
 		command = replaceCommandArgs(command, "testDirectory", baseDir+testDirectory);
 		command = replaceCommandArgs(command, "file.pathSeparator", File.pathSeparator);
 		command = replaceCommandArgs(command, "file.separator", File.separator);
 		
+		
 		return command;
 	}
 	public void execute() throws MojoExecutionException, MojoFailureException {
 		try {
+			
+			
 			includeDirectory = baseDir.toString()
 			+ "/target/include";
+
 			FileHelper.prepareDependencies(includeDirectory, classpathElements);
 			
-			startupCommand = doDefaultReplaces(startupCommand);
-			getLog().info("=====================================================");
-			getLog().info("RUNNING STARTUP COMMAND");
-			getLog().info("=====================================================");
-			if (!"".equals(startupCommand)) {
-				executeCommand(startupCommand);	
+			if (startupCommand!=null && !"".equals(startupCommand)) { 
+				startupCommand = doDefaultReplaces(startupCommand);
+				getLog().info("=====================================================");
+				getLog().info("RUNNING STARTUP COMMAND");
+				getLog().info("=====================================================");
+				executeCommand(startupCommand);
 			}
 			DirectoryWalker walker = new DirectoryWalker();
 			walker.setBaseDir(new File(baseDir.toString()+testDirectory));
@@ -126,6 +141,7 @@ public class ScriptTestRunnerMojo extends AbstractMojo {
 			if (testIncludes!=null)  {
 				walker.setIncludes(testIncludes);	
 			}
+			
 			walker.addDirectoryWalkListener(new DirectoryWalkListener(){
 
 				public void debug(String arg0) {
@@ -143,13 +159,14 @@ public class ScriptTestRunnerMojo extends AbstractMojo {
 				public void directoryWalkStep(int arg0, File arg1) {
 					String execCommand = doDefaultReplaces(command);
 					execCommand = replaceCommandArgs(execCommand, "file", arg1.toString());
+					getLog().debug("file: " + arg1.toString()); 
 					executeCommand(execCommand);
 					
 				}
 				
 			});
 			walker.addSCMExcludes();
-			getLog().info("");
+			
 			getLog().info("=====================================================");
 			getLog().info("START TEST PHASE");
 			getLog().info("=====================================================");
@@ -165,8 +182,8 @@ public class ScriptTestRunnerMojo extends AbstractMojo {
 	
 	private void handleError(String line) {
 		for (int i = 0; errorIdentifiers!=null && i < errorIdentifiers.size(); i++) {
-			if (line!=null && errorIdentifiers.get(i).equals(line.trim())) { 
-				ExecutionError executionError = new ExecutionError();
+			if (line!=null && line.trim().startsWith(errorIdentifiers.get(i))) { 
+				ExecutionError executionError = new ExecutionError("Error identifier matches: "+errorIdentifiers.get(i)+ ". Line: " +line.trim());
 				errors.add(executionError);
 			}
 		}
@@ -177,8 +194,22 @@ public class ScriptTestRunnerMojo extends AbstractMojo {
 		getLog().info("script: " + command);
 		
 		try {
+			
+			Commandline c = new Commandline(command);
+			if (environmentVariables!=null) {
+				Set<Entry<Object, Object>> entrySet = environmentVariables.entrySet();
+				for (Iterator iterator = entrySet.iterator(); iterator
+						.hasNext();) {
+					Entry<Object, Object> entry = (Entry<Object, Object>) iterator
+							.next();
+					String value  =this.doDefaultReplaces((String)entry.getValue());
+					c.addEnvironment((String)entry.getKey(), value );
+					getLog().debug("Adding to env: " + (String)entry.getKey() + " : "+ value );
+				}
+				
+			}
 			CommandLineUtils.executeCommandLine(
-					new Commandline(command), new StreamConsumer() {
+					c, new StreamConsumer() {
 						public void consumeLine(String line) {
 							getLog().info("script.out: " + line);
 							handleError(line);
